a8c4e2847de33b8dd24cc449bd7c5d44db8bc51e
Connor Adams <connorads@users.noreply.github.com>
2021-07-04 21:11:31 +0100

    Complete exercise Unit Tests

--- Lesson 5 - Software Engineering Practices, Part 2/Unit tests/compute_launch.py
+++ Lesson 5 - Software Engineering Practices, Part 2/Unit tests/compute_launch.py
@@ -4,4 +4,5 @@ def days_until_launch(current_day, launch_day):
     current_day (int) - current day in integer
     launch_day (int) - launch day in integer
     """
-    return launch_day - current_day
+    days_left = launch_day - current_day
+    return days_left if days_left > 0 else 0
