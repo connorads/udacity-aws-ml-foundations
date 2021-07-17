52eef7d38bb3fed34c8f0ac90d90c9e4ad41ffd7
Connor Adams <connorads@users.noreply.github.com>
2021-07-17 17:06:13 +0100

    Complete exercise Making a package and pip installing

---
+++ Lesson 6 - Intro to OOP/Making a package and pip installing/3a_python_package/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='connorads-gaussian-package-example',
+      version='1.0',
+      description='An example Python package that does Gaussian distribution stuff',
+      packages=['distributions'],
+      zip_safe=False)
